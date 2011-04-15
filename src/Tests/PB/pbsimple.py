af02c0bbd45002be95db7ab51c475695c8b3e805
Thomas Purchas <tpurchas@gmail.com>
2011-04-15 20:18:56 +0100

    Did a couple of extra experiments. I have expand my spec so far.

--- src/Tests/PB/pbsimple.py
+++ src/Tests/PB/pbsimple.py
@@ -6,10 +6,16 @@
 from twisted.spread import pb
 from twisted.internet import reactor
 
+class Two(pb.Referenceable):
+    def remote_thing(self):
+        print 'Thing called'
+
 class Echoer(pb.Root):
     def remote_echo(self, st):
         print 'echoing:', st
-        return st
+        a = Two()
+        b = Two()
+        return [a, a, b]
 
 if __name__ == '__main__':
     reactor.listenTCP(8789, pb.PBServerFactory(Echoer()))
--- src/Tests/PB/pbsimpleclient.py
+++ src/Tests/PB/pbsimpleclient.py
@@ -6,12 +6,24 @@
 from twisted.spread import pb
 from twisted.internet import reactor
 from twisted.python import util
+import inspect
+
+def thing(a):
+    c = a[2]
+    b = a[1]
+    a = a[0]
+    c.luid = a.luid
+    print a == b
+    print b == c 
+    print inspect.getmembers(a)
+    print inspect.getmembers(b)
+    print inspect.getmembers(c)
 
 factory = pb.PBClientFactory()
 reactor.connectTCP("localhost", 8789, factory)
 d = factory.getRootObject()
 d.addCallback(lambda object: object.callRemote("echo", {'Hello':23, 'Network':'234'}))
-d.addCallback(lambda echo: 'server echoed: ' + repr(echo))
+d.addCallback(thing)
 d.addErrback(lambda reason: 'error: ' + str(reason.value))
 d.addCallback(util.println)
 d.addCallback(lambda _: reactor.stop())
