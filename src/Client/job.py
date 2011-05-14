ccc383d6f25d931fa5c19cac8bf434b16e3a5b2d
Thomas Purchas <tpurchas@gmail.com>
2011-05-14 23:36:22 +0100

    The new job.py file has a new job class in it. It will wrap the object from the server and will manage running Handbrake process etc.

---
+++ src/Client/job.py
@@ -0,0 +1,23 @@
+'''
+Created on 14 May 2011
+
+@author: Thomas Purchas
+'''
+
+class job(object):
+    '''
+    Local job objects. These contain all of the code to coordinate handbrake
+    management and files. 
+    '''
+
+
+    def __init__(self, HBJob):
+        '''
+        Takes a HBJob reference and wraps it up.
+        '''
+        
+        self.raw = HBJob
+        
+        print 'New Job!!'
+    
+    def stop(self):
