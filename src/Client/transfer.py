be68155d85646aab9a4c571d9a6aeab10af06e97
Thomas Purchas <tpurchas@gmail.com>
2011-05-16 21:21:47 +0100

    This should have been part of the previous commit, but never mind. Transfer.py now has a much more useful placeholder in it which describes the first sort of transfer agent I want to try and implement.

--- src/Client/transfer.py
+++ src/Client/transfer.py
@@ -4,14 +4,12 @@ Created on 16 May 2011
 @author: Thomas Purchas
 '''
 
-class MyClass(object):
+def xfer_sockets(destination, service, port):
     '''
-    classdocs
+    This is the basic transfer method that uses sockets to transfer the raw
+    data across the network using sockets. It cheats a little be by using tar
+    files to wrap and compress the data.
     '''
-
-
-    def __init__(selfparams):
-        '''
-        Constructor
-        '''
+    pass
+    
         
