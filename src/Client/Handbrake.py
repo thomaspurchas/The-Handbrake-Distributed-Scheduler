08bfb41c1175b3c64ef3b30905bccc4057ac62b6
Thomas Purchas <tpurchas@gmail.com>
2011-05-16 22:21:29 +0100

    The contense of Handbrake.py has been replaced with some more useful information.

--- src/Client/Handbrake.py
+++ src/Client/Handbrake.py
@@ -4,14 +4,21 @@ Created on 16 May 2011
 @author: Thomas Purchas
 '''
 
-class MyClass(object):
+class Manager(object):
     '''
-    classdocs
+    This is a management class for looking after Handbrake. Inisalising this
+    sets in motion the process of setting up a handbrake process and setting it
+    going. 
     '''
 
 
-    def __init__(selfparams):
+    def __init__(self, args, remoteJob):
         '''
-        Constructor
+        Takes an args argument. This should contain all the necessary arguments
+        to set Handbrake running, and in the same format that a twisted process
+        reactor expects.
+        
+        Also expect the remoteJob object so that is can report its progress
+        directly to the server.
         '''
         
