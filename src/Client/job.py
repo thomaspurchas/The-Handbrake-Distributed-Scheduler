8dbe8b3d8837e43aded57268d7db5382651108f1
Thomas Purchas <tpurchas@gmail.com>
2011-05-16 21:32:28 +0100

    job.py should now be capable of transfering files and starting handbrake. However I am not happy with the granularity of my current aproach, but it will to for V1.

--- src/Client/job.py
+++ src/Client/job.py
@@ -3,6 +3,10 @@ Created on 14 May 2011
 
 @author: Thomas Purchas
 '''
+from twisted.internet import defer
+import tempfile
+import os
+import Handbrake
 
 class job(object):
     '''
@@ -11,14 +15,74 @@ class job(object):
     '''
 
 
-    def __init__(self, HBJob):
+    def __init__(self, service, HBJob):
         '''
         Takes a HBJob reference and wraps it up.
         '''
         
         self.raw = HBJob
+        self.dir = tempfile.mkdtemp()
+        self.original = os.path.join(self.dir, 'original')
+        self.final = os.path.join(self.dir, 'final')
+        
+        self.args = ''
+        
+        self.hbManager = None
+        
+        os.mkdir(self.original)
+        os.mkdir(self.final)
         
         print 'New Job!!'
+    
+    def transferFilesToHere(self):
+        
+        self.raw.callRemote('setStage', 2)
+        d = self.raw.callRemote('serveFiles')
+        d.addCallback(self.service.fileFromServerTo, self.original)
+        
+        return d
+    
+    def getArgs(self):
+        
+        def setArgs(args):
+            self.args = args
+            
+            return None
+        
+        d = self.raw.callRemote('getArgs')
+        d.addCallback(setArgs)
+        
+        return d
+    
+    def startHandbrake(self, _):
+        '''
+        We now have the files and the handbrake arguments. Now time to start 
+        Quattro and get encoding. 
+        '''
+        
+        self.hbManager = Handbrake.Manager(self.args, self.raw)
+        
+        return self.hbManager.start()
+        
+    def start(self):
+        '''
+        Starts this job
+        '''
+        
+        def checkResults(Results):
+            for success, _ in Results:
+                if not success:
+                    raise Exception('REPLACE, The file transfer was not successful')
+                
+            return None
+        
+        transfer = self.transferFilesToHere()
+        args = self.getArgs()
+        
+        dList = defer.DeferredList([transfer, args])
+        
+        dList.addCallback(checkResults)
+        dList.addCallback(self.startHandbrake)
     
     def stop(self):
         print 'Asked to stop job'
