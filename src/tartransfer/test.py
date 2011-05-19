9f69e9a5679f8efd65a81c6712228a3158c378f0
Thomas Purchas <tpurchas@gmail.com>
2011-05-19 23:25:11 +0100

    Some simple test code that links the sender to the client

---
+++ src/tartransfer/test.py
@@ -0,0 +1,19 @@
+'''
+Created on 19 May 2011
+
+@author: Thomas Purchas
+'''
+
+from sender import Sender
+from client import Client
+from twisted.internet import reactor
+
+def stop(_): reactor.stop()
+
+if __name__ == '__main__':
+    C = Client('../Tests/Taroutput')
+    S = Sender()
+    
+    S.beginTransfer(C, '../', '').addCallback(stop)
+    
+    reactor.run()
