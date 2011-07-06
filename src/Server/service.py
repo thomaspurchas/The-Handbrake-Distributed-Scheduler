a98cde810cd3208ca0d7c55d0761d0f05a86e49a
Thomas Purchas <tpurchas@gmail.com>
2011-07-06 20:09:34 +0100

    I have set up the serve and return files callbacks properly, they should now match docs. Also striped out all the spare whitespace.

--- src/Server/service.py
+++ src/Server/service.py
@@ -4,6 +4,7 @@ Created on 23 Apr 2011
 @author: Thomas Purchas
 '''
 from HBClasses import HBJob, HBQueue
+import TarSendFiles
 
 class serverService(object):
     '''
@@ -18,60 +19,65 @@ class serverService(object):
         self.jobs = []
         self.queues = []
         self.clients = []
-    
+
     def newClient(self, Client):
         self.clients.append(Client)
-        
+
     def connecitonFail(self, Failure, Client=None):
         '''
         Something has gone wrong some where. We shall print the traceback and
         kill the client.
         '''
-        
         Failure.printTrackback()
         if Client != None:
             Client.callRemote('shutdown')
-    
-    def serveFolder(self, Folder): pass
-    
+
+    def serveFiles(self, Folder):
+        sender = TarSendFiles.FileSender(Folder)
+        return sender.GetInfo()
+
+    def returnFiles(self, Folder):
+        receiver = TarSendFiles.FileReceiver(Folder)
+        return receiver.GetInfo()
+
     def createJob(self, SomeInfo):
-        
+
         job = HBJob(*SomeInfo)
-        
+
         self.jobs.append(job)
-        
+
         self.queueJobs()
-        
+
     def updateClients(self):
         '''
         Tell all the clients to update there queues.
         '''
-        
+
         for client in self.clients:
-            
+
             client.callRemote('updateQueue').addErrback(self.connectionFail, client)
-        
+
     def createQueue(self, Client):
-        
+
         Q = HBQueue(Client)
-        
+
         self.queues.append(Q)
-        
-        return Q 
-        
+
+        return Q
+
     def queueJobs(self):
-        
+
         for job in self.jobs:
-            
+
             shortest = None
-            
+
             for Q in self.queues:
                 if shortest == None:
                     shortest = Q
                 elif len(Q.jobs) < len(shortest.jobs):
-                    
+
                     shortest = Q
-        
+
             shortest.addJob(job)
-        
+
         self.updateClients()
